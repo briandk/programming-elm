506206990237311d424913487197ed2fec171631
Brian Danielak <briandaniela.k+github@gmail.com>
2022-11-21 21:55:49 -0500

    Display 3 photos on Picshare

--- 01-get-started/02-static-app/src/Picshare.elm
+++ 01-get-started/02-static-app/src/Picshare.elm
@@ -4,16 +4,28 @@ import Html exposing (..)
 import Html.Attributes exposing (class, src)
 
 
+viewDetailedPhoto : String -> String -> Html msg
+viewDetailedPhoto url caption =
+    div [ class "detailed-photo" ]
+        [ img [ src url ] []
+        , div [ class "photo-info" ]
+            [ h2 [ class "caption" ] [ text caption ] ]
+        ]
+
+
+baseUrl : String
+baseUrl =
+    "https://programming-elm.com/"
+
+
 main : Html msg
 main =
     div []
         [ div [ class "header" ]
             [ h1 [] [ text "Picshare" ] ]
         , div [ class "content-flow" ]
-            [ div [ class "detailed-photo" ]
-                [ img [ src "https://programming-elm.com/1.jpg" ] []
-                , div [ class "photo-info" ]
-                    [ h2 [ class "caption" ] [ text "Surfing" ] ]
-                ]
+            [ viewDetailedPhoto (baseUrl ++ "1.jpg") "Surfing"
+            , viewDetailedPhoto (baseUrl ++ "2.jpg") "The Fox"
+            , viewDetailedPhoto (baseUrl ++ "3.jpg") "Evening"
             ]
         ]
